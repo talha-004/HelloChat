9256835cbe8c3812b6ee0acf7ed948d44db6af57
Talha-2023 <syedtalhaahmed123786@gmail.com>
2025-12-16 15:38:35 +0530

    small bug fix

--- backend/src/services/chat.service.ts
+++ backend/src/services/chat.service.ts
@@ -53,7 +53,7 @@ export const getUserChatService = async (userId: string) => {
   const chats = await ChatModel.find({
     participants: { $in: [userId] },
   })
-    .populate("particippants", "name avatar")
+    .populate("participants", "name avatar")
     .populate({
       path: "lastMessage",
       populate: {
